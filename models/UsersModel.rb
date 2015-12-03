2a7f4db72fcea25745f0fc82836c71bca9f01d91
Adriana Castaneda <adriana.lcs316@gmail.com>
2015-12-02 22:05:01 -0600

    change user migration, model, and controller

--- models/UsersModel.rb
+++ models/UsersModel.rb
@@ -1,5 +1,21 @@
+require 'bcrypt'
 class UsersModel < ActiveRecord::Base
   # student.name
   # student.email
   self.table_name = 'users'
+  include BCrypt
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def password
+    BCrypt::Password.new(self.password_digest)
+  end
+
+  def self.authenticate(user_name, password)
+    user = UsersModel.find_by(user_name: user_name)
+    return user if user.password == password
+  end
+
+
 end
